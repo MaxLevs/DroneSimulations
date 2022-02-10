b4c296265548447fd5fd955ad8ad486750af91ac
Maksim Liven <liven.maksim@inlinepro.ru>
2022-02-10 12:04:24 +0300

    Обновил RelyCommand. Сделал параметры nullable

--- DroneSimulations/Common/RelyCommand.cs
+++ DroneSimulations/Common/RelyCommand.cs
@@ -5,10 +5,10 @@ namespace DroneSimulations.Common
 {
     public class RelyCommand : ICommand
     {
-        private readonly Action<object> _execute;
-        private readonly Func<object, bool>? _canExecute;
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
 
-        public RelyCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+        public RelyCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
         {
             _execute = execute;
             _canExecute = canExecute;
@@ -22,12 +22,12 @@ namespace DroneSimulations.Common
 
         public bool CanExecute(object? parameter)
         {
-            return _canExecute?.Invoke(parameter) ?? true;
+            return _canExecute == null || _canExecute(parameter);
         }
 
         public void Execute(object? parameter)
         {
-            _execute.Invoke(parameter);
+            _execute(parameter);
         }
     }
 }
