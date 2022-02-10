fa11fc28e33e34a841d4e1ccddc839b8b31efc80
Maxim Liven <maxlevs@mail.ru>
2022-02-10 23:10:47 +0300

    Добавил метод-расширение для анимации double-проперти объектов типа UIElement

---
+++ DroneSimulations/Common/UiElemntExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace DroneSimulations.Common
+{
+    public static class UiElemntExtensions
+    {
+        /// <summary>
+        /// Сконфигурировать и начать анимацию указанного double-проперти
+        /// </summary>
+        /// <param name="property">Анимируемое проперти</param>
+        /// <param name="toValue">Конечное значение анимации</param>
+        /// <param name="duration">Продолжительность анимации</param>
+        public static void AnimateDoubleProperty(this UIElement associatedObject, DependencyProperty property, double toValue, TimeSpan duration, double? accelRatio, double? decelRatio)
+        {
+            var xOffsetAnimation = new DoubleAnimation(toValue, duration, FillBehavior.Stop)
+            {
+                AccelerationRatio = accelRatio ?? 0.5,
+                DecelerationRatio = decelRatio ?? 0.5
+            };
+
+            EventHandler? OnActionIsOver = null;
+            OnActionIsOver = (sender, args) =>
+            {
+                associatedObject.SetValue(property, toValue);
+
+                xOffsetAnimation.Completed -= OnActionIsOver;
+            };
+
+            xOffsetAnimation.Completed += OnActionIsOver;
+
+            associatedObject.BeginAnimation(property, xOffsetAnimation);
+        }
+    }
+}
