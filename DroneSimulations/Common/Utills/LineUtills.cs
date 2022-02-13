9bf638fb3d6ec9e2ccc26d765570b707ce921161
Maxim Liven <maxlevs@mail.ru>
2022-02-13 13:44:05 +0300

    Добавил координатную сетку

---
+++ DroneSimulations/Common/Utills/LineUtills.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DroneSimulations.Common.Utills
+{
+    public static partial class Utills
+    {
+        public static Line GetHorizontalLine(double x1, double x2, double y)
+        {
+            return new Line { X1 = x1, Y1 = y, X2 = x2, Y2 = y, StrokeThickness=1, Stroke=Brushes.Black };
+        }
+
+        public static Line GetVerticalLine(double x, double y1, double y2)
+        {
+            return new Line { X1 = x, Y1 = y1, X2 = x, Y2 = y2, StrokeThickness=1, Stroke=Brushes.Black };
+        }
+    }
+}
--- DroneSimulations/MVVM/View/DisplayView.xaml.cs
+++ DroneSimulations/MVVM/View/DisplayView.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using DroneSimulations.Common.Utills;
 
 namespace DroneSimulations.MVVM.View
 {
@@ -79,6 +81,91 @@ namespace DroneSimulations.MVVM.View
         public DisplayView()
         {
             InitializeComponent();
+
+            DependencyPropertyDescriptor.FromProperty(XViewPointPositionProperty, typeof(DisplayView))
+                                        .AddValueChanged(this, new EventHandler(AreaPropertyChanged));
+            DependencyPropertyDescriptor.FromProperty(YViewPointPositionProperty, typeof(DisplayView))
+                                        .AddValueChanged(this, new EventHandler(AreaPropertyChanged));
+            DependencyPropertyDescriptor.FromProperty(ScaleProperty, typeof(DisplayView))
+                                        .AddValueChanged(this, new EventHandler(AreaPropertyChanged));
+
+            CoordinateGridElements.SizeChanged += AreaPropertyChanged;
+        }
+
+        public void AreaPropertyChanged(object? sender, EventArgs e)
+        {
+            const double baseDrawShift = 50.0;
+            const double minDrawShift = 25.0;
+
+            const double mainAxisThickness = 2;
+
+            var xOffset = XViewPointPosition * Scale;
+            var yOffset = YViewPointPosition * Scale;
+
+            // CoordinateGridElements.Children.RemoveRange(2, int.MaxValue); // Не удалять две линии, указывающие на текущий вьюпоинт
+            CoordinateGridElements.Children.Clear(); // Удалять всё (убрать вьюпоинт из отображения)
+
+            var width = CoordinateGridElements.ActualWidth;
+            var height = CoordinateGridElements.ActualHeight;
+            var relatedDrawShift = (baseDrawShift * Scale); // % (baseDrawShift - minDrawShift) + minDrawShift; // Цикличность зума
+            var expandedArea = 2 * relatedDrawShift;
+
+            var centerX = width / 2;
+            var centerY = height / 2;
+
+
+            var mainVertical = Utills.GetVerticalLine(centerX - xOffset, 0, height);
+            mainVertical.StrokeThickness = mainAxisThickness;
+            CoordinateGridElements.Children.Add(mainVertical);
+
+            var xDrawShifts = GenerateOffsetsFromCenter(0 - expandedArea, width + expandedArea, relatedDrawShift)
+                                                .Select(x => x - xOffset % relatedDrawShift)
+                                                .ToList();
+            foreach (var xShift in xDrawShifts)
+            {
+                var line = Utills.GetVerticalLine(xShift, 0 - expandedArea, height + expandedArea);
+                CoordinateGridElements.Children.Add(line);
+            }
+
+
+            var mainHorizontal = Utills.GetHorizontalLine(0, width, centerY - yOffset);
+            mainHorizontal.StrokeThickness = mainAxisThickness;
+            CoordinateGridElements.Children.Add(mainHorizontal);
+
+            var yDrawShifts = GenerateOffsetsFromCenter(0 - expandedArea, height + expandedArea, relatedDrawShift)
+                                                .Select(y => y - yOffset % relatedDrawShift)
+                                                .ToList();
+            foreach (var yShift in yDrawShifts)
+            {
+                var line = Utills.GetHorizontalLine(0, width, yShift);
+                CoordinateGridElements.Children.Add(line);
+            }
+        }
+
+        public IEnumerable<double> GenerateOffsetsFromCenter(double startValue, double endValue, double offset)
+        {
+            if (offset <= 0)
+            {
+                throw new ArgumentException("This property must have a positive value", nameof(offset));
+            }
+
+            if (startValue >= endValue)
+            {
+                throw new ArgumentException("startValue must be lower than endValue");
+            }
+
+            var centerValue = (startValue + endValue) / 2.0;
+            var maxOffset = centerValue - startValue;
+
+            yield return centerValue;
+
+            for (var currentOffset = offset; currentOffset < maxOffset; currentOffset += offset)
+            {
+                yield return centerValue + currentOffset;
+                yield return centerValue - currentOffset;
+            }
+
+            yield break;
         }
     }
 }
